571660780d7f9513ac472849789f67da7d809fee
chaotic-existential <Ermakova_ib2.19@st.ithub.ru>
2022-05-08 17:01:02 +0300

    ✨ simple market visualization test1 both tasks

--- data.js
+++ data.js
@@ -1,7 +1,16 @@
-// source.png
-let labels = [
+let labels1 = [
     "TSMC", "UMC", "SMIC", "Samsung", "GlobalFoundries",
     "Hua Hong group", "Vanguard", "PSMC", "Tower Semi", "Others"]
 
-let values = [
+let values1 = [
     28, 13, 11, 10, 7, 6, 6, 6, 5, 8]
+
+let labels2 = []
+
+for (let i = 0; i < 9; i++) {
+    labels2[i] = 1980 + i*5
+}
+
+let values2 = [
+    9.8, 17.6, 33.0, 48.5, 89.1,
+    120.2, 190.8, 235.5, 322.2]
--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 let chart1 = $("#chart1");
+let chart2 = $("#chart2");
 
 new Chart(chart1, {
     type: 'doughnut',
     data: {
-        labels: labels,
+        labels: labels1,
         datasets: [
             {
                 data: [
-                    values[0],
-                    values[1],
-                    values[2],
-                    values[3],
-                    values[4],
-                    values[5],
-                    values[6],
-                    values[7],
-                    values[8],
-                    values[9]],
+                    values1[0],
+                    values1[1],
+                    values1[2],
+                    values1[3],
+                    values1[4],
+                    values1[5],
+                    values1[6],
+                    values1[7],
+                    values1[8],
+                    values1[9]],
                 cutout: '60%',
                 spacing: 2,
                 backgroundColor:
@@ -38,13 +39,66 @@ new Chart(chart1, {
         },
         plugins: {
             title: {
-                text: `По данным Counterpoint за февраль 2021`,
+                text: `Февраль 2021 по данным Counterpoint`,
                 display: true
             },
             legend: {
                 display: true,
                 position: 'right',
                 rtl: true
+            }
+        }
+    }
+});
+
+new Chart(chart2, {
+    type: 'line',
+    data: {
+        labels: labels2,
+        datasets: [
+            {
+                data: [
+                    values2[0],
+                    values2[1],
+                    values2[2],
+                    values2[3],
+                    values2[4],
+                    values2[5],
+                    values2[6],
+                    values2[7],
+                    values2[8]],
+                fill: true,
+                backgroundColor: "rgba(2, 130, 130, 0.05)",
+                // 0.25, если в браузере используется белый фон
+                borderColor: "#04b8b2",
+                borderWidth: 2,
+                pointRadius: 0,
+                pointHitRadius: 100
+            }
+        ]
+    },
+
+    options: {
+        layout: {
+            padding: 20
+        },
+        plugins: {
+            title: {
+                text: `Период с ${labels2[0]} по ${labels2.at(-1)} по данным IC Insights`,
+                display: true
+            },
+            legend: {
+                display: false
+            }
+        },
+        scales: {
+            yAxis: {
+                title: {
+                    text: `миллиарды единиц поставок`,
+                    display: true
+                },
+                suggestedMin: 70,
+                suggestedMax: 450
             }
         }
     }
