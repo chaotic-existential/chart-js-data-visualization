1c60ddad26c4bc42f69dfdda06924c3c75733fd7
chaotic-existential <Ermakova_ib2.19@st.ithub.ru>
2022-05-08 16:31:56 +0300

    ✨ simple semiconductors market visualization test2

--- data.js
+++ data.js
@@ -1,10 +1,7 @@
 // source.png
-let labels = []
-
-for (let i = 0; i < 9; i++) {
-    labels[i] = 1980 + i*5
-}
+let labels = [
+    "TSMC", "UMC", "SMIC", "Samsung", "GlobalFoundries",
+    "Hua Hong group", "Vanguard", "PSMC", "Tower Semi", "Others"]
 
 let values = [
-    9.8, 17.6, 33.0, 48.5, 89.1,
-    120.2, 190.8, 235.5, 322.2]
+    28, 13, 11, 10, 7, 6, 6, 6, 5, 8]
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let chart1 = $("#chart1");
 
 new Chart(chart1, {
-    type: 'line',
+    type: 'doughnut',
     data: {
         labels: labels,
         datasets: [
@@ -15,39 +15,36 @@ new Chart(chart1, {
                     values[5],
                     values[6],
                     values[7],
-                    values[8]],
-                fill: true,
-                backgroundColor: "rgba(2, 130, 130, 0.05)",
-                // 0.25, если в браузере используется белый фон
-                borderColor: "#04b8b2",
+                    values[8],
+                    values[9]],
+                cutout: '60%',
+                spacing: 2,
+                backgroundColor:
+                    ['#29427a', '#225282', '#1d6188', '#20718b', '#2c7e8c',
+                        '#3b8c8e' , '#4a9a8f', '#5ca890', '#72b490', '#8bc191'],
+                borderColor:
+                    ['#172647', '#14314e', '#123c55', '#144757', '#1b5059',
+                        '#25595b' , '#31665f', '#407564', '#518167', '#668e6a'],
                 borderWidth: 2,
-                pointRadius: 0,
-                pointHitRadius: 100
+                borderRadius: 5,
+                hoverOffset: 10
             }
         ]
     },
 
     options: {
         layout: {
-          padding: 20
+            padding: 20
         },
         plugins: {
             title: {
-                text: `Период с ${labels[0]} по ${labels.at(-1)} по данным IC Insights`,
+                text: `По данным Counterpoint за февраль 2021`,
                 display: true
             },
             legend: {
-                display: false
-            }
-        },
-        scales: {
-            yAxis: {
-                title: {
-                    text: `миллиарды единиц поставок`,
-                    display: true
-                },
-                suggestedMin: 70,
-                suggestedMax: 450
+                display: true,
+                position: 'right',
+                rtl: true
             }
         }
     }
