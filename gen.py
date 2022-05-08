8b59548d54a8c852ab926b63b479df0250895ce6
chaotic-existential <Ermakova_ib2.19@st.ithub.ru>
2022-05-08 11:25:49 +0300

    ✨ simple RUB/USD dynamic visualization task3

--- gen.py
+++ gen.py
@@ -1,40 +1,31 @@
 import pandas as pd
 
-src = 'data.xlsx'
-df = pd.read_excel(src, skiprows=2, index_col=0, header=1)
+src = 'data.csv'
+names = ['name', 'interval', 'date', 'time', 'open', 'high', 'low', 'close', 'vol']
 
-# Избавляемся от фиктивных рядов без данных
-for index, data in df.iterrows():
-    if '-' in data.array:
-        df.drop(index=index, inplace=True)
+stonks = pd.read_csv(src, sep='\t', names=names, usecols=['date', 'close'])
+stonks.drop(axis=0, index=0, inplace=True)
 
 # Приводим колонки к нужным типам
-for label, content in df.iteritems():
-    df[label] = df[label].astype('float')
+stonks['close'] = stonks['close'].astype('float')
+stonks['date'] = pd.to_datetime(stonks['date'])
+stonks = stonks.set_index(['date'])
 
-# Вычисляем среднее арифметическое от годовых значений
-df['avg'] = round(df.loc[:, df.columns.array].mean(axis=1))
+# Вытаскиваем последние 10 дней
+stonks = stonks.tail(10)
 
-# Сортируем данные по среднему арифметическому
-df.sort_values(by=['avg'], ascending=False, inplace=True)
-
-# Объединяем (суммируем) неосновные ряды в "Другие"
-df = pd.concat([df.head(9), df.tail(len(df) - 9).agg(['sum'])])
-df.rename(index={'sum': 'Другие'}, inplace=True)
-
-# Сортируем данные по среднему арифметическому ещё раз
-df.sort_values(by=['avg'], ascending=False, inplace=True)
+# Шаманим над форматом даты
+stonks.index = stonks.index.astype(str)
 
 # Вытаскиваем данные из таблицы
-labels = df.index.tolist()
-block1 = df[2019].tolist()
-block2 = df[2020].tolist()
+labels = stonks.index.tolist()
+values = stonks['close'].tolist()
 
 # Помещаем данные в js-массивы
 path = "data.js"
 result = f"""
 let labels = {labels};
-let values = [{block1}, {block2}];"""
+let values = {values};"""
 
 with open(path, 'w', encoding='utf') as file:
     file.write(result)
--- index.js
+++ index.js
@@ -1,37 +1,29 @@
-let chart2019 = $("#chart2019");
-let chart2020 = $("#chart2020");
+let chart1 = $("#chart1");
 
-let pies = [chart2019, chart2020]
-
-pies.forEach((name, i) => new Chart(name, {
-    type: 'doughnut',
+new Chart(chart1, {
+    type: 'line',
     data: {
         labels: labels,
         datasets: [
             {
                 data: [
-                    values[i][0],
-                    values[i][1],
-                    values[i][2],
-                    values[i][3],
-                    values[i][4],
-                    values[i][5],
-                    values[i][7],
-                    values[i][8],
-                    values[i][9],
-                    values[i][10]],
-                cutout: '60%',
-                spacing: 4,
-                rotation: 90,
-                backgroundColor:
-                    ['#602969', '#752d6f', '#8b3271', '#a1386f', '#b73d6b',
-                        '#cc4664' , '#db565d', '#e46c5d', '#e88366', '#eb9973'],
-                borderColor:
-                    ['#311535', '#41193e', '#571f47', '#6d264b', '#832c4d',
-                        '#99344a' , '#a74147', '#b15348', '#b4664f', '#b7775a'],
-                borderWidth: 3,
-                borderRadius: 10,
-                hoverOffset: 10
+                    values[0],
+                    values[1],
+                    values[2],
+                    values[3],
+                    values[4],
+                    values[5],
+                    values[6],
+                    values[7],
+                    values[8],
+                    values[9]],
+                fill: true,
+                backgroundColor: "rgba(100, 220, 10, 0.05)",
+                // 0.25, если в браузере используется белый фон
+                borderColor: "#5EB804",
+                borderWidth: 2,
+                pointRadius: 0,
+                pointHitRadius: 100
             }
         ]
     },
@@ -42,16 +34,19 @@ pies.forEach((name, i) => new Chart(name, {
         },
         plugins: {
             title: {
-                text: `${2019+i} год`,
+                text: `Данные за период с ${labels[0]} по ${labels.at(-1)}`,
                 display: true
             },
             legend: {
-                display: true,
-                reverse: true,
-                position: 'right',
-                rtl: true,
+                display: false
+            }
+        },
+        scales: {
+            yAxis: {
+                suggestedMin: 70,
+                suggestedMax: 80
             }
         }
     }
-}));
+});
 
